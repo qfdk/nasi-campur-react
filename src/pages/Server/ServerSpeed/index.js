b4943692fa56156ab59710899055640dfb2f7078
qfdk <qfdk2010@gmail.com>
2021-07-18 21:35:13 +0200

    fix: WIP 测试

--- src/pages/Server/ServerSpeed/index.js
+++ src/pages/Server/ServerSpeed/index.js
@@ -3,25 +3,33 @@ import httpRequest from '../../../request';
 import {faSyncAlt} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
-const ServerSpeed = (props) => {
-    // const {data} = props;
-    const [servers, setServer] = useState(props.data);
+const getServerInfo = async (server) => {
+    try {
+        const deltas = [];
+        for (let i = 0; i < 3; i++) {
+            const date1 = new Date();
+            await httpRequest.get(server.url);
+            const date2 = new Date();
+            deltas.push(date2 - date1);
+        }
+        return {...server, delta: Math.min(...deltas)};
+    } catch (e) {
+        return {...server, delta: 999};
+    }
+};
 
-    const fetchData = () => {
-        servers.map((server, index) => {
-            const startTime = new Date();
-            httpRequest.get(server.url).then(response => {
-                const endTime = new Date();
-                const delta = endTime - startTime;
-                const updateState = [...servers];
-                const newDate = {...server, delta};
-                updateState[index] = newDate;
-                const newServers = updateState.sort((a, b) => a.delta - b.delta);
-                setServer(newServers);
-            }).catch(e => {
-                console.log(e.message);
-            });
+const ServerSpeed = (props) => {
+    const [servers, setServers] = useState(props.data);
+
+    const fetchData = async () => {
+        const tables = [];
+        for (const server of servers) {
+            tables.push(await getServerInfo(server));
+        }
+        const newServers = tables.sort((a, b) => {
+            return a.delta - b.delta;
         });
+        setServers(newServers);
     };
 
     useEffect(() => {
@@ -50,7 +58,7 @@ const ServerSpeed = (props) => {
                                 <th scope="row">{index}</th>
                                 <td>{data.country}</td>
                                 <td>{data.description}</td>
-                                <td>{data.delta} ms</td>
+                                <td>{data.delta === 999 ? '-' : data.delta + ' ms'}</td>
                             </tr>
                         );
                     })
