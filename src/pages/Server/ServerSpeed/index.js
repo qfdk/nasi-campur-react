84a0f68932cdaf742407e535b13c2f4801d208c2
qfdk <qfdk2010@gmail.com>
2021-07-19 18:31:57 +0200

    fix: 测速排序动画

--- src/pages/Server/ServerSpeed/index.js
+++ src/pages/Server/ServerSpeed/index.js
@@ -2,7 +2,9 @@ import React, {Fragment, useEffect, useReducer, useState} from 'react';
 import httpRequest from '../../../request';
 import {faSyncAlt} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import FlipMove from 'react-flip-move';
 
+const N = 3;
 const Line = React.memo(({number, data}) => {
     return (
         <Fragment>
@@ -29,7 +31,7 @@ const lineReducer = (state, action) => {
             case 'RESET':
                 return {
                     ...state,
-                    data: state.data.map(e => {return {...e, isFinish: false, message: '', delta: '-'};})
+                    data: state.data.map(e => {return {...e, isFinish: false, message: '等待检测...', delta: '-'};})
                 };
             case 'SET_CURRENT':
                 const {newData, currentServerDomain} = action.payload;
@@ -62,13 +64,13 @@ const ServerSpeed = (props) => {
             try {
                 const deltas = [];
                 const cpt = 1;
-                for (let i = 0; i < 5; i++) {
+                for (let i = 0; i < N; i++) {
                     const date1 = new Date();
                     await httpRequest.get(server.url);
                     const date2 = new Date();
                     const delta = date2 - date1;
                     deltas.push(delta);
-                    const newData = {...server, delta, isFinish: (cpt + i) === 5, message: `检测第 ${cpt + i}/5`};
+                    const newData = {...server, delta, isFinish: (cpt + i) === N, message: `检测第 ${cpt + i}/${N}`};
                     lineDispatch({
                         type: 'SET_CURRENT', payload: {
                             currentServerDomain: server.domain,
@@ -76,9 +78,14 @@ const ServerSpeed = (props) => {
                         }
                     });
                 }
-                return {...server, delta: Math.min(...deltas)};
             } catch (e) {
-                return {...server, delta: 9999};
+                const newData = {...server, message: `检测失败`};
+                lineDispatch({
+                    type: 'SET_CURRENT', payload: {
+                        currentServerDomain: server.domain,
+                        newData
+                    }
+                });
             }
         };
 
@@ -115,15 +122,17 @@ const ServerSpeed = (props) => {
                     </tr>
                     </thead>
                     <tbody>
-                    {
-                        lines.data.map((data, index) => {
-                            return (
-                                <tr key={data.domain}>
-                                    <Line number={index + 1} data={data}/>
-                                </tr>
-                            );
-                        })
-                    }
+                    <FlipMove typeName={null}>
+                        {
+                            lines.data.map((data, index) => {
+                                return (
+                                    <tr key={data.domain}>
+                                        <Line number={index + 1} data={data}/>
+                                    </tr>
+                                );
+                            })
+                        }
+                    </FlipMove>
                     </tbody>
 
                 </table>
