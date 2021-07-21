6995c62f56e582aec40b08804332021e47dbca09
qfdk <qfdk2010@gmail.com>
2021-07-21 08:42:36 +0200

    fix: 测试分离

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import loadable from '@loadable/component';
 
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery/dist/jquery.min';
-
-window.$ = $;
-window.jQuery = $;
-loadable(() => require('bootstrap/dist/js/bootstrap.min'));
 
 ReactDOM.render(
     <App/>,
