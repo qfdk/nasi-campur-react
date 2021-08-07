3326aeec9170e63c06afa00efeb3b8d708228e46
qfdk <qfdk2010@gmail.com>
2021-08-07 18:31:59 +0200

    fix: 测试

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import packageJson from '../../../package.json';
 
 const Footer = () => {
     return (
         <div className="container">
             <hr/>
             <footer className="bs-docs-footer">
-                <b>Build</b> with all the ❤ by
-                <a href="https://qfdk.me" target="_blank" rel="noreferrer" > @qfdk</a>
-                <p>Code licensed <a href="https://github.com/twbs/bootstrap/blob/master/LICENSE" target="_blank"
-                                    rel="noreferrer">MIT</a>, docs <a
-                    href="https://creativecommons.org/licenses/by/3.0/" target="_blank" rel="noreferrer">CC BY
-                    3.0</a>, coding with <a href="https://reactjs.org/" rel="noreferrer" target="_blank">React.js</a>
-                </p>
+                <b>Build</b> with all the ❤ by <a href="https://qfdk.me" target="_blank" rel="noreferrer">@qfdk</a>,&nbsp;
+                <b>version</b> : {packageJson.version}
+                <br/>
+                Code licensed <a href="https://github.com/twbs/bootstrap/blob/master/LICENSE" target="_blank"
+                                 rel="noreferrer">MIT</a>, docs <a
+                href="https://creativecommons.org/licenses/by/3.0/" target="_blank" rel="noreferrer">CC BY
+                3.0</a>, coding with <a href="https://reactjs.org/" rel="noreferrer" target="_blank">React.js</a>
             </footer>
         </div>
     );
