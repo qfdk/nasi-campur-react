ca5f70f56efcf2ac3996de38f2ca2a1ddf2d31e2
qfdk <qfdk2010@gmail.com>
2021-08-13 22:43:04 +0200

    fix: css 修改测试

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,18 +3,16 @@ import packageJson from '../../../package.json';
 
 const Footer = () => {
     return (
-        // <div className="container">
-            <footer className="bs-docs-footer">
+        <footer className="bs-docs-footer text-center">
             <hr/>
-                <b>Build</b> with all the ❤ by <a href="https://qfdk.me" target="_blank" rel="noreferrer">@qfdk</a>,&nbsp;
-                <b>version</b> : {packageJson.version}
-                <br/>
-                Code licensed <a href="https://github.com/twbs/bootstrap/blob/master/LICENSE" target="_blank"
-                                 rel="noreferrer">MIT</a>, docs <a
-                href="https://creativecommons.org/licenses/by/3.0/" target="_blank" rel="noreferrer">CC BY
-                3.0</a>, coding with <a href="https://reactjs.org/" rel="noreferrer" target="_blank">React.js</a>
-            </footer>
-        // </div>
+            Code licensed <a href="https://github.com/twbs/bootstrap/blob/master/LICENSE" target="_blank"
+                             rel="noreferrer">MIT</a>, docs <a
+            href="https://creativecommons.org/licenses/by/3.0/" target="_blank" rel="noreferrer">CC BY
+            3.0</a>, coding with <a href="https://reactjs.org/" rel="noreferrer" target="_blank">React.js</a>
+            <br/>
+            <b>Build</b> with all the ❤ by <a href="https://qfdk.me" target="_blank" rel="noreferrer">@qfdk</a>,&nbsp;
+            <b>version</b> : {packageJson.version}
+        </footer>
     );
 };
 
