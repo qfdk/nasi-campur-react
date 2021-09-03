1cd0ec0520b07ba855810d355c81d0f392526c30
qfdk <qfdk2010@gmail.com>
2021-09-03 22:13:36 +0200

    fix: 奇怪的 css

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,10 +5,10 @@ const Home = () => {
     return (
         <Fragment>
             <h3>
+                <span>༼ つ ◕_◕ ༽つ  </span>
                 <span className={"text-primary"}>𝐹𝑅</span>
                 <span className={"text-warning"}>𝒜𝒩</span>
                 <span className={"text-danger"}>𝒞𝐸</span>
-                <span>  ಠ‿↼ 查询</span>
             </h3>
             <p className="alert alert-info">本服务致力于技术交流，切勿用于非法用途</p>
             <SearchInput/>
