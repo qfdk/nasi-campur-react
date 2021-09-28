5b1a03619fef67fff2d45512cf562ceee3f78b48
qfdk <qfdk2010@gmail.com>
2021-09-28 19:58:36 +0200

    fix: 添加 ip 地址检查

--- src/pages/IPCheck/index.js
+++ src/pages/IPCheck/index.js
@@ -1,22 +1,21 @@
 import React, {Fragment} from 'react';
 
 const IPCheck = () => {
-
     return (
         <Fragment>
             <h3>IP 检查</h3>
             <table className={'table'}>
                 <thead>
-                    <tr>
-                        <th>检测点</th>
-                        <th>检测结果</th>
-                    </tr>
+                <tr>
+                    <th>检测点</th>
+                    <th>检测结果</th>
+                </tr>
                 </thead>
                 <tbody>
                 <tr>
                     <td>China Site</td>
                     <td>
-                        <iframe title="china" src="http://cn1.qfdk.me:3001/ip" width="100%" height="30" scrolling="no"
+                        <iframe title="china" src="https://myip.ipip.net/" width="100%" height="30" scrolling="no"
                                 frameBorder="0"
                                 marginHeight="0" marginWidth="0"></iframe>
                     </td>
@@ -34,7 +33,7 @@ const IPCheck = () => {
                 <tr>
                     <td>Google</td>
                     <td>
-                        <iframe title="google" src="http://sspanel.net/ip.php" width="100%" height="30" scrolling="no"
+                        <iframe title="google" src="https://sspanel.net/ip.php" width="100%" height="30" scrolling="no"
                                 frameBorder="0"
                                 marginHeight="0" marginWidth="0"></iframe>
                     </td>
